6a4884b94579da589470d2e81329d4f30d66851f
valencaceresm <caceresmendoza.valentina@gmail.com>
2025-09-15 21:13:04 -0300

    Agrego clase Pedido

---
+++ Pedido.cs
@@ -0,0 +1,23 @@
+public enum EstadoPedido { Pendiente, Entregado, Cancelado }
+public class Pedido
+{
+    private int id;
+    private string observacion;
+    private EstadoPedido estado;//enum para el estado
+    private Cadete cadeteAsignado;
+
+    public int Id => id;
+
+    public string Observacion => observacion;
+    public EstadoPedido Estado { get => estado; set => estado = value; }
+    public Cadete CadeteAsignado { get => cadeteAsignado; set => cadeteAsignado = value; }
+
+    //Constructor
+    public Pedido(int id, string observacion)
+    {
+        this.id = id;
+        this.observacion = observacion;
+        this.estado = EstadoPedido.Pendiente;
+        this.cadeteAsignado = null;
+    }
+}
