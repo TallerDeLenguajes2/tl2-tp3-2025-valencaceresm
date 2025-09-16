502f164ff9db296b8effb2ead5ba6c54699e28d9
valencaceresm <caceresmendoza.valentina@gmail.com>
2025-09-15 21:22:15 -0300

    Agrego clase Cadete

---
+++ Cadete.cs
@@ -0,0 +1,22 @@
+public class Cadete
+{
+    private int id;
+    private string nombre;
+    private string direccion;
+    private string telefono;
+
+    public int Id => id;
+    public string Nombre => nombre;
+    public string Direccion => direccion;
+    public string Telefono => telefono;
+
+
+    //Constructor
+    public Cadete(int id, string nombre, string direccion, string telefono)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+}
